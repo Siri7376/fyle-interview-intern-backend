6ba982e3c493e401c2cc68341030bc8e2c1b3025
Siri7376 <sirishareddy51971@gmail.com>
2025-01-28 11:09:17 +0530

    Update count_assignments_in_each_grade.sql

--- tests/SQL/count_assignments_in_each_grade.sql
+++ tests/SQL/count_assignments_in_each_grade.sql
@@ -1,1 +1,8 @@
 -- Write query to get count of assignments in each grade
+SELECT teacher_id, COUNT(*) AS total_grade_A
+FROM assignments
+WHERE grade = 'A'
+GROUP BY teacher_id
+ORDER BY total_grade_A DESC
+LIMIT 1;
+
